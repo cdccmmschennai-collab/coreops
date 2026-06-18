ab102dd3239cf6a2f70f1bf1bcc2158c55d89bcd
cdccmmschennai-collab <cdccmms.chennai@gmail.com>
2026-06-18 12:33:50 +0530

    tasks redesigned and removed old activity types

--- frontend/src/app/(app)/tasks/page.tsx
+++ frontend/src/app/(app)/tasks/page.tsx
@@ -3,11 +3,11 @@ import { Suspense } from "react";
 import { RequireCapability } from "@/components/auth/require-capability";
 import { TasksView } from "@/features/tasks/components/tasks-view";
 
-export default function MyTasksPage() {
+export default function TasksPage() {
   return (
     <RequireCapability capability="task.view">
       <Suspense>
-        <TasksView mode="mine" />
+        <TasksView routeMode="mine" />
       </Suspense>
     </RequireCapability>
   );
